bdc251a4969c54084625a37ab5c92c311113dc5f
Doug Partridge <doug.partridge@cse-crosscom.com.au>
2022-03-23 15:31:21 +1100

    Put LineApp in a separate file

--- SocketThing/Program.cs
+++ SocketThing/Program.cs
@@ -144,44 +144,5 @@ namespace SocketThing
     }
 
 
-    public class LineAppSession : AppSession<LineAppSession>
-    {
-        protected override void OnSessionStarted()
-        {
-            //this.Send("Welcome to SuperSocket Line Server");
-        }
 
-        protected override void HandleUnknownRequest(StringRequestInfo requestInfo)
-        {
-            this.Send("Unknow request");
-        }
-
-        protected override void HandleException(Exception e)
-        {
-            this.Send("Application error: {0}", e.Message);
-        }
-
-        protected override void OnSessionClosed(CloseReason reason)
-        {
-            //add you logics which will be executed after the session is closed
-            base.OnSessionClosed(reason);
-        }
-    }
-
-
-
-    public class LineRequestParser : IRequestInfoParser<StringRequestInfo>
-    {
-        public StringRequestInfo ParseRequestInfo(string source)
-        {
-            return new StringRequestInfo("line", source, null);
-        }
-    }
-
-    public class LineAppServer : AppServer<LineAppSession>
-    {
-        public LineAppServer() : base(new CommandLineReceiveFilterFactory(System.Text.Encoding.UTF8, new LineRequestParser()))
-        {
-        }
-    }
 }
