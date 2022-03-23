51af8ff9d46c72914a3e81140ad3aa1427c4fe14
Doug Partridge <doug.partridge@cse-crosscom.com.au>
2022-03-23 15:31:19 +1100

    Initial commit

---
+++ SocketThing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketThing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
